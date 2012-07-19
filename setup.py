421ce548e7a9a1bb3488e97848da69481183248b
Travis Kaufman <travis.kaufman@gmail.com>
2012-07-18 23:06:03 -0400

    setup.py: fix error and add py_modules kwarg

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from distutils.core import setup
 
 setup(name='yati',
       version='0.8.0',
-      descript='Yet Another Twitter Interface: A Twitter CLI',
+      description='Yet Another Twitter Interface: A Twitter CLI',
       author='Travis Kaufman',
       author_email='travis.kaufman@gmail.com',
       url='https://github.com/traviskaufman/YATI',
       download_url='https://github.com/traviskaufman/YATI/zipball/master',
-      license='GPL'
+      license='GPL',
+      py_modules=['yati']
 )
