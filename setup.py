ff6de8a861254d4237ac09ef9bff984d9d39dfb3
Travis Kaufman <travis.kaufman@gmail.com>
2012-07-18 23:03:50 -0400

    setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='yati',
+      version='0.8.0',
+      descript='Yet Another Twitter Interface: A Twitter CLI',
+      author='Travis Kaufman',
+      author_email='travis.kaufman@gmail.com',
+      url='https://github.com/traviskaufman/YATI',
+      download_url='https://github.com/traviskaufman/YATI/zipball/master',
+      license='GPL'
+)
