44d9517d76a1b4879cf2080d8fd06aad7ade8100
Travis Kaufman <travis.kaufman@gmail.com>
2012-08-10 22:13:16 -0400

    more like EasyWINstall

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
-
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='yati',
-      version='0.8.0',
+      version='1.0.0dev',
       description='Yet Another Twitter Interface: A Twitter CLI',
       author='Travis Kaufman',
       author_email='travis.kaufman@gmail.com',
       url='https://github.com/traviskaufman/YATI',
       download_url='https://github.com/traviskaufman/YATI/zipball/master',
       license='GPL',
+      packages=find_packages(),
+      install_requires=['tweepy==1.10'],
+      entry_points={
+          'console_scripts': [
+              'yati = yati:main'
+          ]
+      },
       py_modules=['yati'],
-      scripts=['bin/yati']
+      long_description=open('README.md').read()
 )
